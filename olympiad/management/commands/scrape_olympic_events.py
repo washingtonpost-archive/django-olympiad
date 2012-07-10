011e6a50ac16665907b4b130501a9b8da4ff5c9a
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-07-10 11:39:48 -0400

    Oooh, debugging. I suck at Django.

---
+++ olympiad/management/commands/scrape_olympic_events.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+from django.core.management.base import BaseCommand
+from olympiad import Event, Athlete, Country, OlympicGame, Sport
+
+class Command(BaseCommand):
+
+    olympic_ids = []
+
+    def handle(self, *args, **kwargs):
+
+        self.build_olympic_id_list()
+
+    def build_olympic_id_list(self):
+        all_games_url = u'http://www.olympic.org/olympic-games'
+        request = requests.get(all_games_url)
+        soup = BeautifulSoup(request.content)
+
+        link_list = soup.select('iocRiaContent').select('li').select('a')
+        print link_list
