ee60bb9836415c360e64c6b821b173575693a625
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-07-10 10:36:53 -0400

    First things first.

---
+++ olympiad/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+##
+# django-olympiad
+##
+__title__ = 'olympiad'
+__version__ = '0.0.1'
+__author__ = 'Dan Hill and Jeremy Bowers'
+__license__ = 'MIT'
+__author_email__ = ['dan.hill@washpost.com', 'bowersj@washpost.com']
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import olympiad
+
+setup(
+        name='django-olympiad',
+        version=olympiad.__version__,
+        description='A reusable Django app for scraping, storing and aggregating Olympic medal counts.',
+        author=olympiad.__author__,
+        author_email=['bowersj@washpost.com', 'dan.hill@washpost.com'],
+        url='https://github.com/wpmedia/django-olympiad',
+        packages=[
+            'olympiad'
+        ],
+        install_requires=[
+            'beautifulsoup4',
+            'requests',
+            'Django>=1.4',
+            'django-tastypie'
+        ],
+        license=olympiad.__license__,
+        classifiers=[
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Utilities'
+        ],
+)
